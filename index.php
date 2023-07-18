5dba0d3f6de077368327e4fdebf337e153abb413
chriz27 <fni.chriz27@gmail.com>
2023-07-18 15:47:50 +0700

    menambah baris paragraf di file index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hallo Word</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nesciunt, pariatur eveniet dolores dolor amet nobis! Ex, asperiores. Adipisci deleniti explicabo temporibus porro eos sint. Eius deleniti modi repellendus ducimus obcaecati, hic perferendis nobis iusto perspiciatis. Eligendi obcaecati eum non ipsum autem, corporis neque consequuntur molestiae unde rem eos. Iusto?</p>
 </body>
 </html>
